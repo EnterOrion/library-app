a2aeee553df88338e48271c484fd004c7d75978e
EnterOrion <orionwesson@gmail.com>
2022-12-13 16:51:51 +0100

    Add table to display books added

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 let myLibrary = [];
 const form = document.getElementById("add_book");
+const table = document.getElementById("library_table");
+
 
 const title = form.elements["book_name"];
 const author = form.elements["author_name"];
 const pages = form.elements["page_number"];
 const read = form.elements["read_validity"];
+
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   addBook();
+  displayLibrary();
   clearForm();
-  console.log(myLibrary);
 });
 
 function Book(title, author, pages, read) {
@@ -32,4 +36,18 @@ function clearForm() {
   title.value = "";
   author.value = "";
   pages.value = "";
+}
+
+function displayLibrary() {
+    const row = table.insertRow(1);
+    const title_cell = row.insertCell(0);
+    const author_cell = row.insertCell(1);
+    const pages_cell = row.insertCell(2);
+    const completed_cell = row.insertCell(3);
+    
+    title_cell.innerHTML = title.value;
+    author_cell.innerHTML = author.value;
+    pages_cell.innerHTML = pages.value;
+    completed_cell.innerHTML = read.value;
+
 }
